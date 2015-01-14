975fb898edbde9142ea7159b90afe7d4b4e3f3bd
Ramon De La Torre <delatorre.ramon66@gmail.com>
2015-01-14 15:45:54 -0800

    Commit for portfolio 3, #3

--- public_html/script.js
+++ public_html/script.js
@@ -4,35 +4,50 @@
  * and open the template in the editor.
  */
 
-$("document").ready(function(){
-     $(".Nike").css("background-color","Gray");
-     $('p:last').css({"background-color": "Red", "color": "white"});
-     $('p:first').css({"background-color": "Green", "color": "white"});
-    
+$("document").ready(function() {
+    $(".Nike").css("background-color", "Gray");
+    $('p:last').css({"background-color": "Red", "color": "white"});
+    $('p:first').css({"background-color": "Green", "color": "white"});
+
     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe);
-    
-    //$('div').bind('moseover', mouseOverMe());
-    
+
+    $('div').bind('moseover', mouseOverMe());
+
     $('h1').bind('click', mouseClick);
-    
-        $('replaceWHtml').bind('click', replaceWHtml);
-        $('replaceWText').bind('click', replaceWText);
+
+    $('#replaceWText').bind('click', replaceWText);
+    $('#randPara').bind('click', addAPara);
+    $('#removePara').bind('click', removeAPara);
+    $('#show').css('visibility', 'hidden');
         
+    
 });
-        function replaceWHtml() {
-            $('')
-        }
+
+function removeAPara() {
+    $('#randPara p:last').remove();
+}
+
+function addAPara() {
+    $('#randPara').append('<p>ADDED</p>');
+}
+
+function replaceWText() {
+    $('#replaceWText').text('Replaced!');
+}
+
+
+
 
 
 function mouseOverMe() {
-    $("h1"). html("Ramon De La Torre");
+    $("h1").html("Ramon De La Torre");
 }
 
-function mouseOutMe(){
-    $('h1').html('Paris Saint Germain');    
+function mouseOutMe() {
+    $('h1').html('Paris Saint Germain');
 }
 
-function mouseClick(){
+function mouseClick() {
     $('p').html('Ramon is beast at Soccer');
 }
 
